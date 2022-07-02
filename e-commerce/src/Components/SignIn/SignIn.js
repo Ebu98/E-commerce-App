2a01a75d086a95f4f52c41798a2bc46bf4432c85
Ebu98 <91473642+Ebu98@users.noreply.github.com>
2022-07-02 17:51:12 +0100

    added redux, and implemented userreducer and userAction to the header and App components

--- e-commerce/src/Components/SignIn/SignIn.js
+++ e-commerce/src/Components/SignIn/SignIn.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import "./SignIn.Style.scss";
 import FormInput from "../Form-Inputs/FormInput";
 import CustomButton from "../Custom-button/CustomButton";
-import { signInWithGoogle } from "../../firebase/firebase.utils";
+import { auth,signInWithGoogle } from "../../firebase/firebase.utils";
 
 export class SignIn extends Component {
   constructor(props) {
@@ -13,15 +13,30 @@ export class SignIn extends Component {
       password: "",
     };
   }
-  handleChange = (e) => {
-    const { value, name } = e.target;
+  
+  handleChange = event => {
+    const { name, value } = event.target;
     this.setState({ [name]: value });
   };
-  handleSubmit = (e) => {
+
+
+  handleSubmit =async e => {
     e.preventDefault();
-    this.setState({ email: "", password: "" });
+    const { email, password} = this.state;
+
+    try {
+      await auth.signInWithEmailAndPassword(
+        email,
+        password
+      );
+      this.setState({ email: "", password: "" });
+    } catch (error) {
+      console.log(error);
+    }
+    
   };
   render() {
+    const { email, password } = this.state;
     return (
       <div className="sign-in">
         <h1>I already have an account</h1>
@@ -31,7 +46,7 @@ export class SignIn extends Component {
           <FormInput
             type="email"
             name="email"
-            value={this.state.email}
+            value={email}
             handleChange={this.handleChange}
             label="email"
             required
@@ -40,7 +55,7 @@ export class SignIn extends Component {
           <FormInput
             type="password"
             name="password"
-            value={this.state.password}
+            value={password}
             handleChange={this.handleChange}
             label="password"
             required
--- e-commerce/src/Routes/index.js
+++ e-commerce/src/Routes/index.js
@@ -3,45 +3,38 @@ import { Route, Routes } from "react-router-dom";
 import ShopPage from "../Pages/Shop/ShopPage";
 import Header from "../Components/header/Header";
 import SignupAndSigninPage from "../Pages/Sign-upAndSign-in Page/Sign-up And Sign-in-Page";
-import { auth, createUserProfileDocument} from "../firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "../firebase/firebase.utils";
+import { connect } from "react-redux";
+import { setCurrentUser } from "../Components/redux/user/user.actions";
 import React from "react";
 
 
 class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      currentUser: null,
-    };
-  }
+  unsubscribeFromAuth = null;
 
-  unsubscribeFormAuth = null;
-
-  
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+    const { setCurrentUser } = this.props;
+
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
         const userRef = await createUserProfileDocument(userAuth);
 
-        userRef.onSnapshot(snapShot => {
-          this.setState({
-            currentUser: {
-              id: snapShot.id,
-              ...snapShot.data()
-            }
+        userRef.onSnapshot((snapShot) => {
+          setCurrentUser({
+            id: snapShot.id,
+            ...snapShot.data(),
           });
-
-          console.log(this.state);
         });
       }
 
-      this.setState({ currentUser: userAuth });
+      setCurrentUser(userAuth);
     });
   }
 
   componentWillUnmount() {
     this.unsubscribeFromAuth();
   }
+
   render() {
     return (
       <div>
@@ -55,4 +48,9 @@ class App extends React.Component {
     );
   }
 }
-export default App;
+
+const mapDispatchToProps = (dispatch) => ({
+  setCurrentUser: (user) => dispatch(setCurrentUser(user)),
+});
+
+export default connect(null, mapDispatchToProps)(App);
